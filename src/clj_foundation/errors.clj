62b2be18cba0810f65e0fe5329657908b4c3c489
David Orme <dorme@bradsdeals.com>
2016-06-15 13:02:49 -0500

    Moved error check boilerplate code->errors.clj

--- src/clj_foundation/errors.clj
+++ src/clj_foundation/errors.clj
@@ -8,40 +8,12 @@
   (:gen-class))
 
 
-(s/defn replace-nil :- s/Any
-  "Accepts a value that cannot be nil; if it is not nil it returns it, else it
-  returns its replacement."
-  [maybe-nil :- s/Any, replacement :- s/Any]
-  (if (nil? maybe-nil)
-    replacement
-    maybe-nil))
-
-
-(s/defn not-nil :- s/Any
-  "If value is not nil, returns it, else throws IllegalArgumentExceoption with
-  the message \"${name} cannot be nil\""
-  [value :- s/Any, name :- s/Str]
-  (if (nil? value)
-    (throw (java.lang.IllegalArgumentException. (str name " cannot be nil")))
-    value))
-
-
-
-(defprotocol ComputationFailed
-  "A protocol that determines if a computation has resulted in a failure.
-   This allows the definition of what constitutes a failure to be extended
-   to new types by the consumer."
-  (failure? [self]))
-
-(extend-protocol ComputationFailed
-  Object
-  (failure? [self] false)
-
-  Throwable
-  (failure? [self] true)
-
-  nil
-  (failure? [self] false))
+(s/defn exception-seq :- [Throwable]
+  "Returns a lazy sequence of all nested exceptions starting with the top-level exception and working to
+   the initial exception by recursively unrolling the (.getCause exception) for each nested exception.."
+  [exception :- Throwable]
+  (if exception
+    (lazy-seq (cons exception (exception-seq (.getCause exception))))))
 
 
 (s/defn expect-within :- s/Any
@@ -60,6 +32,23 @@
               (Thread/sleep 500)
               (recur (predicate)))
             completed))))))
+
+
+(defprotocol ComputationFailed
+  "A protocol that determines if a computation has resulted in a failure.
+   This allows the definition of what constitutes a failure to be extended
+   to new types by the consumer."
+  (failure? [self]))
+
+(extend-protocol ComputationFailed
+  Object
+  (failure? [self] false)
+
+  Throwable
+  (failure? [self] true)
+
+  nil
+  (failure? [self] false))
 
 
 (defmacro try*
@@ -89,6 +78,7 @@ considered errors.  After tries attempts, the last error is returned."
           (Thread/sleep pause-millis)
           (recur (dec tries) pause-millis f args))))))
 
+
 (defn retry
   "Retry calling the specified function f & args while pausing pause-millis
 between attempts.  Uncaught exceptions are considered errors.  After tries
@@ -116,3 +106,65 @@ attempts, the last caught exception is re-thrown."
   The remaining parameters (if any) are that function's arguments."
   [tries pause-millis]
   (fn [f & args] (apply retry tries pause-millis f args)))
+
+
+(s/defn replace-if :- s/Any
+  "In the binary form, replace value with the result of running predicate against value
+   if that result is truthy.  In the terniary form, replace value with replacement if
+   the result of running predicate against value is truthy.
+
+   An idiomatic way to use this function is to pass a set of disallowed values as
+   predicate, and a replacement value as replacement."
+
+  ([value :- s/Any, predicate :- (=> s/Any [s/Any])]
+   (let [maybe-result (predicate value)]
+     (if maybe-result
+       maybe-result
+       value)))
+
+  ([value :- s/Any, predicate :- (=> s/Any [s/Any]), replacement :- s/Any]
+   (if (predicate value)
+     value
+     replacement)))
+
+
+(s/defn replace-nil :- s/Any
+  "Accepts a value that cannot be nil; if it is not nil it returns it, else it
+  returns its replacement."
+  [maybe-nil :- s/Any, replacement :- s/Any]
+  (replace-if maybe-nil #{nil} replacement))
+
+
+(s/defn not-nil :- s/Any
+  "If value is not nil, returns it, else throws IllegalArgumentExceoption with
+  the message \"${name} cannot be nil\""
+  [value :- s/Any, name :- s/Str]
+  (if (nil? value)
+    (throw (java.lang.IllegalArgumentException. (str name " cannot be nil")))
+    value))
+
+
+(s/defn value-or :- s/Any
+  "If value is nil or Nothing, run f and return its result.  Else, return value."
+  [value :- s/Any, f :- (=> s/Any [s/Any])]
+  (if (#{nil nothing} value)
+    (f value)
+    value))
+
+
+(s/defn something-or :- s/Any
+  "If (something? value) return value, else run f and return its result."
+  [value :- s/Any, f :- (=> s/Any [s/Any])]
+  (if (something? value)
+    value
+    (f value)))
+
+
+(s/defn throw-or :- s/Any
+  "If value is a failure, wrap and throw it in an IllegalStateException
+  with the specified message, else run function on the value and return the
+  result"
+  [value :- s/Any, message :- s/Str, f :- (=> s/Any [s/Any])]
+  (cond
+    (failure? value) (throw (IllegalStateException. message value))
+    :else            (f value)))
