61b554b4bf4d301af49ab444bc4bed5e5f1743a7
David Orme <dorme@bradsdeals.com>
2016-06-17 16:24:03 -0500

    WIP: A NS for CLojure ObJects

---
+++ src/clj_foundation/cloj.clj
@@ -0,0 +1,6 @@
+(ns clj-foundation.cloj
+  "Clojure ObJects.  A peace treaty between OO and FP.  More specifically, a prototype-based
+  object model that integrates with Java's notion of an object/class while also behaving the
+  way Clojure expects."
+  (:require [clj-foundation.patterns :refer :all]
+            [clj-foundation.caller :refer :all]))
