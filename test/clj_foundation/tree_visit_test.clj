b1419634ac1b6eeb4485cc23df290ddccdaae369
David Orme <dorme@bradsdeals.com>
2016-09-21 20:02:47 -0500

    Test tree-visit

--- test/clj_foundation/tree_visit_test.clj
+++ test/clj_foundation/tree_visit_test.clj
@@ -1,18 +1,13 @@
-(ns clj-foundation.pipe-test
-  "A Unix-style pipe operator and recursive grep command.  Pipe operates on a single
-  container's elements.  Grep recursively searches nested data structures.
-
-  Also includes xreduce, a version of reduce that returns a transducer function."
+(ns clj-foundation.tree-visit-test
   (:require [clojure.zip :as z]
             [clojure.core.reducers :as r]
             [clojure.test :refer :all]
-            [bradsdeals.pipe :refer :all]
-            [bradsdeals.tree-visit :as v])
+            [clj-foundation.tree-visit :refer :all])
   (:import [java.util Map Map$Entry]))
 
 (def testdata {:a {:a 1 :b 2 :c {:aa 3 :bb 4 :cc {:aaa [1 2 3 4 5] :bbb "blah!"}}}})
 
-(def cursor (v/tree-zipper testdata))
+(def cursor (tree-zipper testdata))
 
 (defn cursor-seq-inf
 	([] (cursor-seq-inf cursor))
@@ -63,55 +58,51 @@
 
 ;---------------------------------------------------------------------------------------------------------
 
-(defn next [[breadcrumb cursor]]
+(defn next-b [[breadcrumb cursor]]
   (let [loc (z/next cursor)]
-    [(update-breadcrumb breadcrumb loc) loc]))
-
-(defn down [[breadcrumb cursor]]
-  (let [loc (z/down cursor)]
     [(update-breadcrumb breadcrumb loc) loc]))
 
 (deftest test-update-breadcrumb-root
   (is (= [] (update-breadcrumb [] cursor))))
 
 (deftest test-update-breadcrumb-root-next
-  (let [[result _] (-> [[] cursor] next)]
+  (let [[result _] (-> [[] cursor] next-b)]
     (is (= [] result))))
 
 (deftest test-update-breadcrumb-firstchild
-  (let [[result _] (-> [[] cursor] next next)]
+  (let [[result _] (-> [[] cursor] next-b next-b)]
     (is (= [:a] result))))
 
 (deftest test-update-breadcrumb-level2
-  (let [[result _] (-> [[] cursor] next next next next next)]
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b)]
     (is (= [:a :a] result))))
 
 (deftest test-update-breadcrumb-level2b
-  (let [[result _] (-> [[] cursor] next next next next next next next next)]
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b next-b next-b next-b)]
     (is (= [:a :b] result))))
 
 (deftest test-update-breadcrumb-level3a
-  (let [[result _] (-> [[] cursor] next next next next next next next next
-                     next next next next next next)] ;; go to :aa
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b)] ;; go to :aa
     (is (= [:a :c :aa] result))))
 
 (deftest test-update-breadcrumb-level3c
-  (let [[result _] (-> [[] cursor] next next next next next next next next
-                     next next next next next next
-                     next next next next next next)]
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b)]
     (is (= [:a :c :cc] result))))
 
 (deftest test-update-breadcrumb-level4a
-  (let [[result _] (-> [[] cursor] next next next next next next next next
-                     next next next next next next
-                     next next next next next next
-                     next next next next)]
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b)]
     (is (= [:a :c :cc :aaa] result))))
 
 (deftest test-update-breadcrumb-level5
-  (let [[result _] (-> [[] cursor] next next next next next next next next
-                     next next next next next next
-                     next next next next next next
-                     next next next next
-                     next)]
+  (let [[result _] (-> [[] cursor] next-b next-b next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b next-b next-b
+                     next-b next-b next-b next-b
+                     next-b)]
     (is (= [:a :c :cc :aaa 0] result))))
